2f4f247ec95f6f2a252afa4b68119aa39461f474
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-03-27 16:50:09 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -21,7 +21,7 @@ class StudentsController < ApplicationController
   end
 
   def index
-    @students = Student.all
+    @students = Student.search(@ask = params[:ask])
   end
 
   def student_params
