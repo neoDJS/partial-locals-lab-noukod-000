d50cd55af6e2ef045a8c4581d42ee05aae1aec29
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-03-27 03:18:02 +0000

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,12 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(name)
+    if name.empty?
+      self.all
+    else
+      self.where("name like ?", "%#{name}%")
+    end
+  end
 end
